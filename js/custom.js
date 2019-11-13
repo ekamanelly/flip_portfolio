49020a00991eacf37e1b8afc57e38561a653b98e
ekama <ekamanelly@gmail.com>
2019-11-13 21:39:18 +0100

    add final sttyle

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+const front = document.getElementById('profile__front');
+const back = document.getElementById('profile__back');
+
+
+
+
+ document.querySelector('#contact').addEventListener('click',
+ function(){
+   back.classList.toggle('back-to-0');
+   back.classList.toggle('back-to-180');
+   front.classList.toggle('front-to-180');
+
+
+
+
+ }
+
+)
